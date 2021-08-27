7b7495bc348538b6abe2a24a5f017b5a4eddbad7
RomanIgorevich1 <votmoyapochta1@bk.ru>
2021-08-27 11:47:08 +0300

    Глупый бот [#31323]

---
+++ test/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class DummyBotTest {
+
+    @Test
+    public void whenGreetBot() {
+        String in = "Привет, Бот.";
+        String expected = "Привет, умник.";
+        String result = DummyBot.answer(in);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenByeBot() {
+        String in = "Пока.";
+        String expected = "До скорой встречи.";
+        String result = DummyBot.answer(in);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        String in = "Какое сегодня число?";
+        String expected = "Это ставит меня в тупик. Задайте новый вопрос.";
+        String result = DummyBot.answer(in);
+        Assert.assertEquals(expected, result);
+    }
+}
