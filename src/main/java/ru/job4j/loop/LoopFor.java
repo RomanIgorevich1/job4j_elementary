a5770f1d3e047fcb6c8d1a4b6a1c9d94a96e9fb8
RomanIgorevich1 <votmoyapochta1@bk.ru>
2021-09-22 22:37:57 +0300

    Цикл for. Определение. [#3572]

---
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class LoopFor {
+    public static void main(String[] args) {
+
+        int x;
+
+        for (x = 5; x <= 10; x++) {
+            System.out.println(x);
+        }
+
+    }
+}
