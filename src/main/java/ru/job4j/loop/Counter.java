06ad08bb327566f1075a6bbe7dd1b08326dc7f2e
RomanIgorevich1 <votmoyapochta1@bk.ru>
2021-09-28 11:31:51 +0300

    Сумма чисел. [#226416 #209280]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -3,22 +3,11 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        int rsl;
-        for (rsl = start; rsl <= finish; rsl++) {
+
+        for (int rsl = start; rsl <= finish; rsl++) {
             sum = sum + rsl;
         }
         return sum;
-    }
-
-    public static int sumByEven(int start, int finish) {
-        int sum = 0;
-        int rsl;
-        for (rsl = start; rsl <= finish; rsl++) {
-            if (rsl % 2 == 0) {
-                sum = sum + rsl;
-            }
-        } return sum;
-
     }
 
     public static void main(String[] args) {
@@ -26,9 +15,6 @@ public class Counter {
         System.out.println(sum(3, 8));
         System.out.println(sum(1, 1));
 
-        System.out.println(sumByEven(0, 10));
-        System.out.println(sumByEven(3, 8));
-        System.out.println(sumByEven(1, 1));
     }
 }
 
