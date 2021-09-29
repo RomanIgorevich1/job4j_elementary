8f4432b42a99034a832c28f4b8dde66eca18c53c
RomanIgorevich1 <votmoyapochta1@bk.ru>
2021-09-29 16:42:58 +0300

    Простые числа [#156314]

---
+++ src/test/java/ru/job4j/loop/PrimeNumberTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class PrimeNumberTest {
+
+    @Test
+    public void when5() {
+        int finish = 5;
+        int expected = 3;
+        int rsl = PrimeNumber.calc(finish);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void when11() {
+        int finish = 11;
+        int expected = 5;
+        int rsl = PrimeNumber.calc(finish);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void when2() {
+        int finish = 2;
+        int expected = 1;
+        int rsl = PrimeNumber.calc(finish);
+        Assert.assertEquals(expected, rsl);
+    }
+}
