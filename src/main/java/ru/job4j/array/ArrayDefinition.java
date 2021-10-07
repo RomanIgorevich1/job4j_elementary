0513dd88eb133a9ae3f89938eed5872d8008c547
RomanIgorevich1 <votmoyapochta1@bk.ru>
2021-10-07 16:31:48 +0300

    Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,6 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен " + surname.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Роман";
+        System.out.println(names[0]);
+        names[1] = "Игорь";
+        System.out.println(names[1]);
+        names[2] = "Светлана";
+        System.out.println(names[2]);
+        names[3] = "Вероника";
+        System.out.println(names[3]);
     }
 
 }
