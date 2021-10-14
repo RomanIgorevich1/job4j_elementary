acc3ec731d5299371c2b7f4a3a0896c6faa5085a
RomanIgorevich1 <votmoyapochta1@bk.ru>
2021-10-14 19:56:09 +0300

    Поиск минимального числа в массиве. [#179803]

---
+++ src/test/java/ru/job4j/array/MinTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class MinTest {
+
+    @Test
+    public void whenFirsMin() {
+        int[] array = {0, 5, 10};
+        int expected = 0;
+        int result = Min.findMin(array);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenLastMin() {
+        int[] array = {10, 5, 3};
+        int expected = 3;
+        int result = Min.findMin(array);
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenMiddleMin() {
+        int[] array = {10, 2, 5};
+        int expected = 2;
+        int result = Min.findMin(array);
+        Assert.assertEquals(expected, result);
+    }
+}
